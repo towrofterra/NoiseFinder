2c5c37e41c330ab8c1d66e95c283975a93a6411e
Jake Levi <39222075+towrofterra@users.noreply.github.com>
2019-08-28 17:17:39 -0400

    Original files
    Testing

--- js/main.js
+++ js/main.js
@@ -33,13 +33,11 @@
             check=false;
         }
 
-		email = $(email).val().trim();
-        if(email.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null
-		|| email.substr(email.len - 13) != 'husky.neu.edu'){
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
             showValidate(email);
             check=false;
         }
-		console.log($(email).val().trim().substring(email.len - 13));
 
         if($(message).val().trim() == ''){
             showValidate(message);
